aa0d75fddec007962298f5d51f3927b260d4c453
Jop09 <Vanderman@gmx.de>
2016-02-25 15:45:28 +0100

    Fertige Tests

--- NorrisTest.java
+++ NorrisTest.java
@@ -1,29 +1,64 @@
 package NorrisCoding;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Enclosed.class)
 public class NorrisTest {
 
+	@RunWith(Parameterized.class)
+	public static class ParameterizedTest {
 
+		private String text;
+		private String norris;
 
-	//Norris norristest = new Norris();
-	String CC_ascii = "10000111000011";
-	String CC_norris = "0 0 00 0000 0 000 00 0000 0 00";
+		public ParameterizedTest(String givenText, String expectedNorris) {
+			this.text = givenText;
+			this.norris = expectedNorris;
+		}
 
+		@Parameters
+		public static Collection<String[]> createNorrisCodeParameters() {
+			return Arrays
+					.asList(new String[][] { { "", "" } ,
+						{ "CC", "0 0 00 0000 0 000 00 0000 0 00" },
+							{ "C", "0 0 00 0000 0 00" }, { "%",
+									"00 0 0 0 00 00 0 0 00 0 0 0" },
+					{ "Chuck Norris' keyboard has 2 keys: 0 and white space.",
+							"0 0 00 0000 0 0000 00 0 0 0 00 000 0 000 00 0 0 0 00 0 0 000 00 000 0 0000 00 0 0 0 00 0 0 00 00 0 0 0 00 00000 0 0 00 00 0 000 00 0 0 00 00 0 0 0000000 00 00 0 0 00 0 0 000 00 00 0 0 00 0 0 00 00 0 0 0 00 00 0 0000 00 00 0 00 00 0 0 0 00 00 0 000 00 0 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 000 00 000 0 0 00 0 0 00 00 0 0 000000 00 0000 0 0000 00 00 0 0 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 00 00 0 0 0 00 000 0 00 00 0000 0 0000 00 00 0 00 00 0 0 0 00 000000 0 00 00 00 0 0 00 00 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 0000 00 00 0 00 00 0 0 000 00 0 0 0 00 00 0 0 00 000000 0 00 00 00000 0 0 00 00000 0 00 00 0000 0 000 00 0 0 000 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 000 00 0 0 00000 00 0 0 0 00 000 0 00 00 0 0 0 00 00 0 0000 00 0 0 0 00 00 0 00 00 00 0 0 00 0 0 0 00 0 0 0 00 00000 0 000 00 00 0 00000 00 0000 0 00 00 0000 0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0" } });
+		}
 
-	@Test
-	public void testasciiUmwandlung() {
-		System.out.println("Inside testasciiUmwandlung()");
-		assertEquals(CC_ascii, Norris.asciiUmwandlung("CC"));
+		@Test
+		public void testNorris() {
+			assertEquals(norris, Norris.createChuckNorrisCode(Norris.createAsciiCode(text)));
+		}
 	}
 
-	@Test
-	public void testNorrisCode() {
-		System.out.println("Inside testNorrisCode()");
-		assertEquals(CC_norris, Norris.NorrisCode(CC_ascii));
-	}
 
+	public static class StandardTest {
+
+		@Test
+		public void testCreateAsciiCode() {
+			assertEquals("0100101", Norris.createAsciiCode("%"));
+		}
+
+		@Test
+		public void testCreateChuckNorrisCode() {
+			assertEquals("00 0 0 0 00 00 0 0 00 0 0 0", Norris.createChuckNorrisCode("0100101"));
+		}
+
+		@Test
+		public void testAddZeros() {
+			assertEquals("0100101", Norris.addZeros("100101"));
+		}
+	}
 
 }
